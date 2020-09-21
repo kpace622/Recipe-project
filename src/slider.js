71fab585f7abea33acb2288cce10a10e3845c216
Kyle Pace <kpace2296@gmail.com>
2020-09-21 17:00:10 -0500

    working on home page

--- src/slider.js
+++ src/slider.js
@@ -7,28 +7,32 @@ import image4 from './assets/robin-stickel-tzl1UCXg5Es-unsplash.jpg';
 const Slider = () => {
 
   return (
-    <div>
-      <div className='slide-container'>
-          <a href="#slide-1">1</a>
-          <a href="#slide-2">2</a>
-          <a href="#slide-3">3</a>
-          <a href="#slide-4">4</a>
-      </div>
-      <div className='slider'>
+    <div className='home-container'>
+      <h2>Store all your recipes in one place!</h2>
+      <div className='slider-container'>
+        <div className='slide-container'>
+            <a href="#slide-1">1</a>
+            <a href="#slide-2">2</a>
+            <a href="#slide-3">3</a>
+            <a href="#slide-4">4</a>
+        </div>
+        <div className='slider'>
 
-        <div className='slides'>
-          <img id="slide-1" src={image1} alt='food'/>
+          <div className='slides'>
+            <img id="slide-1" src={image1} alt='food'/>
+          </div>
+          <div className='slides'>
+            <img id="slide-2" src={image2} alt='food'/>
+          </div>
+          <div className='slides'>
+            <img id="slide-3" src={image3} alt='food'/>
+          </div>
+          <div className='slides'>
+            <img id="slide-4" src={image4} alt='food'/>
+          </div>
         </div>
-        <div className='slides'>
-          <img id="slide-2" src={image2} alt='food'/>
         </div>
-        <div className='slides'>
-          <img id="slide-3" src={image3} alt='food'/>
-        </div>
-        <div className='slides'>
-          <img id="slide-4" src={image4} alt='food'/>
-        </div>
-      </div>
+      <h2>Add, edit or delete your recipes!</h2>
     </div>
   )
 }
